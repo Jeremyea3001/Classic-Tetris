4745098734333a99fd17c8581a10e47ff4e3d540
Jeremyea3001 <151014662+Jeremyea3001@users.noreply.github.com>
2024-02-12 22:42:31 +0100

    Update Main.py
    Ajout des rotations

--- Main.py
+++ Main.py
@@ -140,13 +140,6 @@ def pose_piece(plateau: list, lst: list) -> bool :
     return False
 
 
-def rotation_piece(coord_piece: list, piece: str, dir: str) -> list :
-    """Modifie les coordonnées de piece avec la rotation dir."""
-    if piece == "T" :
-        if dir == "Clockwise" :
-            pass
-
-
 def rafraichir_plateau(plateau) -> int :
     """Modifie le plateau si une ligne est remplie et renvoie True. Renvoie False dans le cas contraire."""
     line_clears = 0
@@ -161,6 +154,55 @@ def rafraichir_plateau(plateau) -> int :
     return line_clears
 
 
+def creation_lst_modifie(lst_coords: list, piece: str) -> tuple :
+    """Renvoie la liste modifié avec comme origine le deuxième ou troisième point de la liste."""
+    lst_modifie = list()
+
+    if piece in ["T", "S", "J"] :
+        for x, y in lst_coords :
+            lst_modifie.append((x - lst_coords[1][0], y - lst_coords[1][1]))
+            origine = lst_coords[1]
+
+    elif piece in ["I", "Z", "L"] :
+        for x, y in lst_coords :
+            lst_modifie.append((x - lst_coords[2][0], y - lst_coords[2][1]))
+            origine = lst_coords[2]
+        
+    return lst_modifie, origine
+
+
+def rotation(lst_coords: list, piece: str, dir: str) -> list :
+    """Renvoie la liste des coordonnées modifié avec la rotation dans le sens dir."""
+
+    lst_modifie, origine = creation_lst_modifie(lst_coords, piece)
+    lst_coords_modifie = list()
+    lst_resultat = list()
+
+    for x, y in lst_modifie :
+        if dir == "Clockwise" :
+            lst_coords_modifie.append((y, -x))
+        else :
+            lst_coords_modifie.append((-y, x))
+    
+    for x, y in lst_coords_modifie :
+        lst_resultat.append((x + origine[0], y + origine[1]))
+
+    return lst_resultat
+
+
+
+def rotation_piece(coord_piece: list, piece: str, dir: str) -> list :
+    """Modifie les coordonnées de piece avec la rotation dir s'il n'y a aucun obstacles."""
+    lst = list()
+
+    if piece == "O" :
+        return coord_piece
+
+    lst = rotation(coord_piece, piece, dir)
+
+    if not all(plateau[i][j] for i, j in lst) and all(x >= 0 for x, y in lst) :
+        return lst
+    return coord_piece
 
 
 
